01657fa0e70e2c7525a50c8c5563815d89d2f5d2
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-16 08:43:16 -0500

    Ran into errors with files. Updated to previous downloads. Created file api file paths.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,18 @@
-//Loading data
+// //Loading data
 
-const noteData = require('../db/db.json');
+// const noteData = require('../db/db.json');
 
-module.exports = (app) => {
+// module.exports = (app) => {
 
-    app.get('/api/notes', (req, res) => res.json(noteData));
+//     app.get('/api/notes', (req, res) => res.json(noteData));
 
-    app.post('/api/notes', (req, res) => {
-        // test to see how response works
-        res.send('Got a post request');
-    });
+//     app.post('/api/notes', (req, res) => {
+//         // test to see how response works
+//         res.send('Got a post request');
+//     });
 
-    app.delete('/api/notes/:id', (req, res) => {
-        // test to see how response works
-        res.send('Got a delete request at this id')
-    });
-};
+//     app.delete('/api/notes/:id', (req, res) => {
+//         // test to see how response works
+//         res.send('Got a delete request at this id')
+//     });
+// };
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const db = require('./db/db.json');
 
 const app = express();
 
@@ -9,10 +12,70 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// Router
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+//Static route used for getting css file
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get("/assets/js/index.js", function(req, res) {
+//     res.sendFile(path.join(__dirname, "public/assets/js/index.js"));
+// });
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+
+});
+
+// app.get('/index', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// })
+
+//determining if this path is required as well
+// app.get('/api/notes',(req,res) => {
+//     res.sendFile(path.join(__dirname, 'api/notes.html'));
+
+// });
+
+
+//testing object db.json parsing
+//confirmed that db is being loaded by require
+//console.log(db);
+
+const fileDB = fs.readFileSync('./db/db.json');
+
+const fileData = JSON.parse(fileDB);
+
+console.log(fileData); // returning the object
+
+
+// let newDB = JSON.parse(db);
+// console.log(newDB);
+
+
+//API Routes
+// app.get("/api/notes", (req,res) =>{
+//     res.sendFile(path.join((__dirname,"./db/db.json")))
+//     // let fileDB = fs.readFileSync(db,'utf-8');
+//     // let fileData = JSON.parse(fileDB);
+//     // res.json(fileData);
+//     // // return res.json(db)
+//     // // res.json({title:"Test Title",text:"Test text"})
+// })
+
+app.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+})
+
+app.post('/api/notes', (req,res) =>{
+    const newNote = req.body;
+   // console.log(newNote);
+});
+
+
 
 //Listener
 app.listen(PORT, () => {
